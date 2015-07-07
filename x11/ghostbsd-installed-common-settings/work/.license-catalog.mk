2eed1c58b8bd702d31b7c18a0921961977770908
ericbsd <ericturgeon.bsd@gmail.com>
2015-07-06 21:35:44 -0300

    Updatight ghostbsd-installed-common-settings to 1.5

---
+++ x11/ghostbsd-installed-common-settings/work/.license-catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=BSD
+_LICENSE_NAME=BSD license Generic Version (deprecated)
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI COPYFREE
+_LICENSE_DISTFILES=GhostBSD-ghostbsd-installed-common-settings-1.5_GH0.tar.gz
