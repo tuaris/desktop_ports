f2c7481bfa52e7096291ec9fa30c4a22204ee0db
ericbsd <ericturgeon.bsd@gmail.com>
2015-11-30 21:44:30 -0400

    updating gbi to 2.13

---
+++ sysutils/gbi/work/gbi-2.13/src/installType.py
@@ -0,0 +1,91 @@
+#!/usr/local/bin//python
+#
+# Copyright (c) 2015 GhostBSD
+#
+# See COPYING for licence terms.
+#
+# type.py v 0.5 Thursday, Mar 28 2013 19:31:53 Eric Turgeon
+#
+# type.py create and delete partition slice for GhostBSD system.
+
+from gi.repository import Gtk
+import os
+import os.path
+
+# Folder use pr the installer.
+tmp = "/home/ghostbsd/.gbi/"
+installer = "/usr/local/lib/gbi/"
+query = "sh /usr/local/etc/lib/backend-query/"
+if not os.path.exists(tmp):
+    os.makedirs(tmp)
+
+logo = "/usr/local/lib/gbi/logo.png"
+disk_file = '%sdisk' % tmp
+boot_file = '%sboot' % tmp
+signal = '%ssignal' % tmp
+disk_list = '%sdisk-list.sh' % query
+disk_info = '%sdisk-info.sh' % query
+part_query = '%sdisk-part.sh' % query
+
+
+class Types():
+
+    def partition(self, radiobutton, val):
+        self.ne = val
+        pass_file = open(signal, 'w')
+        pass_file.writelines(self.ne)
+        pass_file.close
+        return
+
+    def get_type(self):
+        return self.ne
+
+    def get_model(self):
+        return self.box1
+
+    def __init__(self):
+        self.box1 = Gtk.VBox(False, 0)
+        self.box1.show()
+        box2 = Gtk.VBox(False, 10)
+        box2.set_border_width(10)
+        self.box1.pack_start(box2, True, True, 0)
+
+        box2.show()
+        # auto partition or Customize Disk Partition.
+        bbox = Gtk.VBox()
+        label = Gtk.Label()
+        box2.pack_start(label, False, False, 0)
+        label = Gtk.Label(
+            '<b><span size="xx-large">Installation Type</span></b>')
+        label.set_use_markup(True)
+        box2.pack_start(label, False, False, 10)
+        # create a Hbox to center the radio button.
+        hbox = Gtk.HBox()
+        box2.pack_start(hbox, True, True, 10)
+        radio = Gtk.RadioButton.new_with_label_from_widget(None, "UFS full disk Configuration")
+        bbox.pack_start(radio, False, True, 10)
+        radio.connect("toggled", self.partition, "disk")
+        self.ne = 'disk'
+        pass_file = open(signal, 'w')
+        pass_file.writelines(self.ne)
+        pass_file.close
+        radio.show()
+        # box2.pack_start(radio, True, True, 10)
+        radio = Gtk.RadioButton.new_with_label_from_widget(radio, "UFS Custom Configuration")
+        bbox.pack_start(radio, False, True, 10)
+        radio.connect("toggled", self.partition, "custom")
+        radio.show()
+        # box2.pack_start(radio, True, True, 10)
+        radio = Gtk.RadioButton.new_with_label_from_widget(radio, "ZFS Configuration")
+        bbox.pack_start(radio, False, True, 10)
+        radio.connect("toggled", self.partition, "zfs")
+        radio.show()
+        hbox.pack_start(bbox, True, True, 100)
+        # hbox.pack_start(bbox, True, True, 50)
+        label = Gtk.Label()
+        box2.pack_start(label, False, False, 0)
+        box2 = Gtk.HBox(False, 10)
+        box2.set_border_width(5)
+        self.box1.pack_start(box2, False, False, 0)
+        box2.show()
+        return
---
+++ sysutils/gbi/work/stage/usr/local/lib/gbi/install.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 GhostBSD
+#
+# See COPYING for licence terms.
+#
+# install.py v 0.4 Sunday, February 08 2015 Eric Turgeon
+#
+# install.py give the job to pc-sysinstall to install GhostBSD.
+from gi.repository import Gtk, GObject
+from gi.repository import WebKit
+import threading
+import locale
+import os
+from subprocess import Popen, PIPE, STDOUT, call
+from time import sleep
+from partition_handler import rDeleteParttion, destroyParttion, makingParttion
+from create_cfg import gbsd_cfg
+
+tmp = "/home/ghostbsd/.gbi/"
+gbi_path = "/usr/local/lib/gbi/"
+sysinstall = "/usr/local/sbin/pc-sysinstall"
+rcconfgbsd = "/etc/rc.conf.ghostbsd"
+encoding = locale.getpreferredencoding()
+utf8conv = lambda x: str(x, encoding).encode('utf8')
+threadBreak = False
+GObject.threads_init()
+
+
+def close_application(self, widget):
+    Gtk.main_quit()
+
+
+def read_output(command, probar):
+    call('service hald stop', shell=True)
+    call('umount /media/GhostBSD', shell=True)
+    probar.set_fraction(0.004)
+    probar.set_text("Creating pcinstall.cfg")
+    if os.path.exists(rcconfgbsd):
+        gbsd_cfg()
+    probar.set_text("Partition table Configuration")
+    sleep(2)
+    if os.path.exists(tmp + 'delete'):
+        probar.set_fraction(0.004)
+        probar.set_text("Deleting partition")
+        rDeleteParttion()
+        sleep(2)
+    # destroy disk partition and create scheme
+    if os.path.exists(tmp + 'destroy'):
+        probar.set_fraction(0.005)
+        probar.set_text("Creating new disk with partitions")
+        destroyParttion()
+        sleep(2)
+    # create partition
+    if os.path.exists(tmp + 'create'):
+        probar.set_fraction(0.008)
+        probar.set_text("Creating new partitions")
+        makingParttion()
+        sleep(2)
+    probar.set_text("Beginning installation")
+    sleep(2)
+    p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE,
+              stderr=STDOUT, close_fds=True)
+    while 1:
+        line = p.stdout.readline()
+        if not line:
+            break
+        new_val = probar.get_fraction() + 0.000003
+        probar.set_fraction(new_val)
+        bartext = line
+        probar.set_text("%s" % bartext.rstrip())
+        ## Those for next 4 line is for debugin only.
+        # filer = open("/home/ghostbsd/.gbi/tmp", "a")
+        # filer.writelines(bartext)
+        # filer.close
+        print(bartext)
+    probar.set_fraction(1.0)
+    call('service hald start',shell=True)
+    if bartext.rstrip() == "Installation finished!":
+        Popen('python %send.py' % gbi_path, shell=True, close_fds=True)
+        call("rm -rf /home/ghostbsd/.gbi/", shell=True, close_fds=True)
+        GObject.idle_add(window.destroy)
+    else:
+        Popen('python %serror.py' % gbi_path, shell=True, close_fds=True)
+        GObject.idle_add(window.destroy)
+
+
+class Installs():
+    default_site = "/usr/local/lib/gbi/slides/welcome.html"
+
+    def close_application(self, widget):
+        Gtk.main_quit()
+
+    def __init__(self, button1, button2, button3, notebook):
+        self.box1 = Gtk.VBox(False, 0)
+        self.box1.show()
+        box2 = Gtk.VBox(False, 10)
+        box2.set_border_width(10)
+        self.box1.pack_start(box2, True, True, 0)
+        box2.show()
+        self.pbar = Gtk.ProgressBar()
+        self.pbar.set_fraction(0.0)
+        self.pbar.set_size_request(-1, 20)
+        box2.pack_start(self.pbar, False, False, 0)
+        web_view = WebKit.WebView()
+        web_view.open(self.default_site)
+        sw = Gtk.ScrolledWindow()
+        sw.add(web_view)
+        sw.show()
+        box2.pack_start(sw, True, True, 0)
+        command = '%s -c %spcinstall.cfg' % (sysinstall, tmp)
+        thr = threading.Thread(target=read_output,
+                               args=(command, self.pbar))
+        thr.setDaemon(True)
+        thr.start()
+        return
+
+    def get_model(self):
+        return self.box1
