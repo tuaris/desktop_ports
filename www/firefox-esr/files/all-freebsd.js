1178255abdfb5d3ae26590a6b70cf7c77be76c06
Daniel Morante <daniel@morante.net>
2020-08-16 04:09:21 -0400

    Disable Pocket, DoH, and Telemetry by default in Firefox ports.

---
+++ www/firefox-esr/files/all-freebsd.js
@@ -0,0 +1,7 @@
+// FreeBSD-specific defaults overrides
+pref("app.shield.optoutstudies.enabled", false);
+pref("app.normandy.enabled",false);
+pref("browser.safebrowsing.enabled", false);
+pref("browser.safebrowsing.malware.enabled", false);
+pref("extensions.pocket.enabled", false);
+pref("network.trr.mode", 5);
