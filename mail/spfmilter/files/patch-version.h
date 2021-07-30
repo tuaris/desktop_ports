1d7dec47252f9291de89a76c34ca2e3c7b12bca4
Daniel Morante <daniel@morante.net>
2021-07-30 03:29:07 -0400

    Add mail/spfmilter

---
+++ mail/spfmilter/files/patch-version.h
@@ -0,0 +1,13 @@
+--- version.h.orig	2015-02-25 23:05:55 UTC
++++ version.h
+@@ -3,8 +3,8 @@
+ #ifndef _VERSION_H_
+ #define _VERSION_H_
+ 
+-#define SPFMILTER_PROGRAM "spfmilter"
++#define SPFMILTER_PROGRAM "SPF Validation"
+ #define SPFMILTER_VERSION "2.001"
+-#define SPFMILTER_URL "http://www.acme.com/software/spfmilter/"
++#define SPFMILTER_URL "http://pacyworld.com/spf"
+ 
+ #endif /* _VERSION_H_ */
